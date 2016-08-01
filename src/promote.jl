81a8b1eae3729b73b166f1fec1a0b8cd5ff08446
anj1 <alireza.n.j@gmail.com>
2016-08-01 15:41:33 +1200

    Make type promotion system more Julian

--- src/promote.jl
+++ src/promote.jl
@@ -1,6 +1,6 @@
 # conversions and promotions from integer and rational types to algebraic number types
 import Base.convert 
-import Base.promote
+import Base.promote_rule
 
 # Algebraic number from integer
 convert{T<:Integer}(::Type{AlgebraicNumber}, x::T) = 
@@ -10,10 +10,8 @@ convert{T<:Integer}(::Type{AlgebraicNumber}, x::Rational{T}) =
     AlgebraicNumber(BigInt[-num(x), den(x)], Complex{BigFloat}(x))
 
 # promotions
-promote{T<:Integer}(x::T, y::AlgebraicNumber) = (AlgebraicNumber(x),y)
-promote{T<:Integer}(x::AlgebraicNumber,y::T) = (x,AlgebraicNumber(y))
-promote{T<:Integer}(x::Rational{T}, y::AlgebraicNumber) = (AlgebraicNumber(x),y)
-promote{T<:Integer}(x::AlgebraicNumber,y::Rational{T}) = (x,AlgebraicNumber(y))
+promote_rule{T<:Integer,S,F}(x::Type{T},          y::Type{AlgebraicNumber{S,F}}) = AlgebraicNumber
+promote_rule{T<:Integer,S,F}(x::Type{Rational{T}},y::Type{AlgebraicNumber{S,F}}) = AlgebraicNumber
 
 # conversions back
 function convert(::Type{Int64},an::AlgebraicNumber)
